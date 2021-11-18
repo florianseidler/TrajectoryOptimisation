c361732ba92ff9144b105c82dd330881a01c6bac
florianseidler <93659674+florianseidler@users.noreply.github.com>
2021-11-18 15:35:06 +0100

    Added: Gaussintegration, Tests; Corrected: Zerofunction

--- NLPsolverSingleConstraint.py
+++ NLPsolverSingleConstraint.py
@@ -6,20 +6,22 @@ from jax import grad
 # goal: minimize costfunction to calc the smallest
 # torque-integral needed for trajectory optimisation
 
-# questions:
-# integrationconstant, numerical, boundaries
-
-# TODO: check if costfunction is convex
-# TODO: programm costfunction as integration of torquefunction
-# TODO: boundaries t0=starttime, tf=endtime, x0=startplace, xf=endplace
-
 # example:
 # f(x) = 3 * x² + 2-> min       # template: f(x) = amplitude[0] * x^order + offset
 # x <= 4                        # template: x <= constantOfIneqConstraint
 
 
+# QUESTIONS: integrationconstant, numerical, boundaries
+
+# TODO: check if costfunction is convex
+# TODO: programm costfunction as integration of torquefunction
+# TODO: boundaries t0=starttime, tf=endtime, x0=startplace, xf=endplace
+
+# IDEA: Use integrationgaussquad5 to minimize torquefunction
+
+
 def nlpsolver(order, amplitude, offset, constantOfIneqConstraint):
-    
+
     ineqvar = 0                                                                     # 4. constraint passed
     kkt_x = thirdconstraintparameter(order, amplitude, offset)
     if (checkinequalityconstraint(kktpoint, constantOfIneqConstraint) == 1):        # 2. constraint
@@ -43,8 +45,20 @@ def torquefunction(x, exp, ampli, offs):
     return result
 
 
-def nullfunction(exp, ampli, offs):
-    return (offs) / ampli[exp - 1]
+def nullfunction(exp, ampli, offs):                                                 # Newton’s Method to approx zero
+    iterations = 10
+    initialguess = 1.4
+    approximation = initialguess - (torquefunction(initialguess, exp, ampli, offs) / grad(torquefunction)(initialguess, exp, ampli, offs))
+    for i in range(iterations):
+        approximation = approximation - (torquefunction(approximation, exp, ampli, offs) / grad(torquefunction)(approximation, exp, ampli, offs))
+    return approximation
+
+
+def integrationgaussquad5(fct, lowerlimit, upperlimit):                             # gaussian 5-point quadrature
+    x5point = np.array([-0.90618, -0.538469, 0, 0.538469, 0.90618])
+    w5point = np.array([0.236927, 0.478629, 0.568889, 0.478629, 0.236927])
+    val = (upperlimit - lowerlimit) * (x5point + 1) / 2.0 + lowerlimit
+    return (upperlimit - lowerlimit) / 2.0 * np.sum(w5point * fct(val))
 
 
 def thirdconstraintparameter(ampli, exp, offs):
