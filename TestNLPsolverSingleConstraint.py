3ef6f0ca324f4d7e64bc538ea93e31883c887bac
florianseidler <93659674+florianseidler@users.noreply.github.com>
2021-11-18 15:35:32 +0100

    Gausstest

--- TestNLPsolverSingleConstraint.py
+++ TestNLPsolverSingleConstraint.py
@@ -2,11 +2,19 @@ import pytest
 import numpy as np
 import NLPsolverSingleConstraint
 
-# content of NLPsolverSingleConsraint.py
-exponent = 2
-ampli = np.array([1, 1])
-offs = 0
-constantOfIneqConstraint = 4
 
-def testnlpsolver(exponent, ampli, offs, constantOfIneqConstraint):
-    assert nlpsolver(exponent, ampli, offs, constantOfIneqConstraint) == -(1/2)
+def testnlpsolver():
+    exponent = 2
+    ampli = np.array([1, 1])
+    offs = 0
+    constantOfIneqConstraint = 4
+    assert (nlpsolver(exponent, ampli, offs, constantOfIneqConstraint) == -(1/2))
+
+
+def testintegrationgauss5point():
+    def fct(x):
+        return 2 * x
+    low = 2
+    up = 5
+    assert (21.5 > gaussquad5(fct, low, up) < 20.5)
+
