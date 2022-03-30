efe031e113ff72fbfac2deb99dae9b0e6a29c003
florianseidler <93659674+florianseidler@users.noreply.github.com>
2022-03-30 10:36:43 +0200

    80 columns

--- nlp_jax_ipm/search_direction_calculation.py
+++ nlp_jax_ipm/search_direction_calculation.py
@@ -1,18 +1,21 @@
 import jax.numpy as jnp
-from jax import grad, hessian, jacfwd, lax, jit
-from gradient_and_hessian import gradient_analytic, hessian_approximation, regularize_hessian
+from jax import grad, jit
+from gradient_and_hessian import hessian_approximation, regularize_hessian
 
 
-def calc_search_dir(objective_function, weights, slacks, lagrange_multipliers, num_weights, num_equality_constraints,
-                    num_inequality_constraints, diagonal_shift_val, init_diagonal_shift_val, armijo_val, power_val,
+def calc_search_dir(objective_function, objective_function_with_barrier,
+                    weights, slacks, lagrange_multipliers,
+                    num_weights, num_equality_constraints,
+                    num_inequality_constraints, diagonal_shift_val,
+                    init_diagonal_shift_val, armijo_val, power_val,
                     barrier_val):
 
     """
     Compute primal-dual direction (Nocedal & Wright 19.12)
-
     Parameters
     ----------
     objective_function
+    objective_function_with_barrier
     weights
     slacks
     lagrange_multipliers
@@ -24,26 +27,38 @@ def calc_search_dir(objective_function, weights, slacks, lagrange_multipliers, n
     armijo_val
     power_val
     barrier_val
-
     Returns
     -------
     search_direction
     """
 
-    gradient = gradient_analytic(objective_function, weights, slacks, lagrange_multipliers, barrier_val)
+    gradient_ = jit(grad(objective_function_with_barrier, [0, 1, 2]))(
+        weights, slacks, lagrange_multipliers, barrier_val)
+    gradient_weights = jnp.asarray(gradient_[0])
+    gradient_slacks = jnp.asarray(gradient_[1])
+    gradient_lagrange_multipliers = jnp.asarray(gradient_[2])
+    gradient = jnp.concatenate(
+        [gradient_weights, gradient_slacks, -gradient_lagrange_multipliers],
+        axis=0)
 
-    hessian_approx = hessian_approximation(objective_function, weights, slacks, lagrange_multipliers)
+    hessian_approx = hessian_approximation(
+        objective_function, weights, slacks, lagrange_multipliers)
 
-    hessian_regularized, diagonal_shift_val = \
-        regularize_hessian(hessian_approx, num_weights, num_equality_constraints, num_inequality_constraints,
-                           diagonal_shift_val, init_diagonal_shift_val, armijo_val, power_val, barrier_val)
+    hessian_regularized, diagonal_shift_val = (
+        regularize_hessian(hessian_approx, num_weights,
+                           num_equality_constraints, num_inequality_constraints,
+                           diagonal_shift_val, init_diagonal_shift_val,
+                           armijo_val, power_val, barrier_val))
     # calculate search_direction
-    search_direction = jnp.array(jit(jnp.linalg.solve)(hessian_regularized, -gradient.reshape((gradient.size, 1)))
-                                 .reshape((gradient.size,)))  # reshape gradient and result
+    gradient_transposed = - gradient.reshape((gradient.size, 1))
+    search_direction = jit(jnp.linalg.solve)(
+        hessian_regularized, gradient_transposed)
+    search_direction = jnp.array(search_direction.reshape((gradient.size,)))
 
     if num_inequality_constraints or num_equality_constraints:
         # change sign definition for the multipliers' search direction
-        search_direction = search_direction.at[num_weights + num_inequality_constraints:].set(
-            -search_direction[num_weights + num_inequality_constraints:])
+        search_direction = (
+            search_direction.at[num_weights + num_inequality_constraints:]
+            .set(-search_direction[num_weights + num_inequality_constraints:]))
 
     return search_direction
