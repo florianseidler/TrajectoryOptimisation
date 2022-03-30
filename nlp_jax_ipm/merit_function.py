0576f0c5eb7e27e603f79ba945537f2702f94ac4
florianseidler <93659674+florianseidler@users.noreply.github.com>
2022-03-30 10:34:29 +0200

    80 columns

--- nlp_jax_ipm/merit_function.py
+++ nlp_jax_ipm/merit_function.py
@@ -1,11 +1,13 @@
 import jax.numpy as jnp
-from jax import grad, hessian, jacfwd, lax, jit
+from jax import jit
 
 
-def merit_function(cost_function, weights, slacks, merit_function_parameter, barrier_val, equality_constraints=None,
-                   inequality_constraints=None, num_equality_constraints=0, num_inequality_constraints=0):
+def merit_function(cost_function, weights, slacks, merit_function_parameter,
+                   barrier_val, equality_constraints=None,
+                   inequality_constraints=None, num_equality_constraints=0,
+                   num_inequality_constraints=0):
     """
-    Calculates the merit function used for the step search to minimize the Lagrangian.
+    Calculates merit function used for step search to minimize Lagrangian.
 
     Parameters
     ----------
@@ -25,39 +27,92 @@ def merit_function(cost_function, weights, slacks, merit_function_parameter, bar
     """
 
     if num_equality_constraints and num_inequality_constraints:
-        equality_sum = sum_equality_values(equality_constraints, weights, num_equality_constraints)
-        inequality_sum = sum_inequality_values(inequality_constraints, weights, slacks, num_inequality_constraints)
-        constraints_val = merit_function_parameter * (equality_sum + inequality_sum)
-        return cost_function(weights) + constraints_val - barrier_function(slacks, barrier_val)
+        equality_sum = sum_equality_values(
+            equality_constraints, weights, num_equality_constraints)
+        inequality_sum = sum_inequality_values(
+            inequality_constraints, weights, slacks, num_inequality_constraints)
+        constraints_val = merit_function_parameter * (equality_sum
+                                                      + inequality_sum)
+        return (cost_function(weights) + constraints_val
+                - barrier_function(slacks, barrier_val))
 
     elif num_equality_constraints:
-        equality_sum = sum_equality_values(equality_constraints, weights, num_equality_constraints)
+        equality_sum = sum_equality_values(
+            equality_constraints, weights, num_equality_constraints)
         return cost_function(weights) + merit_function_parameter * equality_sum
 
     elif num_inequality_constraints:
-        inequality_sum = sum_inequality_values(inequality_constraints, weights, slacks, num_inequality_constraints)
-        return cost_function(weights) + merit_function_parameter * inequality_sum - barrier_function(slacks, barrier_val)
+        inequality_sum = sum_inequality_values(
+            inequality_constraints, weights, slacks, num_inequality_constraints)
+        return (cost_function(weights)
+                + merit_function_parameter * inequality_sum
+                - barrier_function(slacks, barrier_val))
     else:
         return cost_function(weights)
 
 
 @jit
 def barrier_function(slacks, barrier_val):
-    """ Calculates the barrier function. """
+    """
+    Calculates the barrier function.
+
+    Parameters
+    ----------
+    slacks
+    barrier_val
+
+    Returns
+    -------
+    barrier function
+    """
+
     return barrier_val * jnp.sum(jnp.log(slacks))
 
 
-def sum_inequality_values(inequality_constraints, weights, slacks, num_inequality_constraints):
-    """ Calculates the sum of the inequality constraints with inserted weights and slacks. """
-    sum = 0
+def sum_inequality_values(inequality_constraints, weights, slacks,
+                          num_inequality_constraints):
+    """
+    Calculates the sum of the inequality constraints with inserted weights
+    and slacks.
+
+    Parameters
+    ----------
+    inequality_constraints
+    weights
+    slacks
+    num_inequality_constraints
+
+    Returns
+    -------
+    sum of the inequalities with inserted weights and slacks
+    """
+
+    loss = 0
     for iter_var in range(num_inequality_constraints):
-        sum += jnp.abs(inequality_constraints[iter_var](weights) - slacks[iter_var])
-    return sum
+        loss += jnp.abs(inequality_constraints[iter_var](weights)
+                        - slacks[iter_var])
+    return loss
 
 
-def sum_equality_values(equality_constraints, weights, num_equality_constraints):
-    """ Calculates the sum of the equality constraints with inserted weights. """
-    sum = 0
-    for iter_var in range(num_equality_constraints):
-        sum += jnp.abs(equality_constraints[iter_var](weights))
-    return sum
+def sum_equality_values(equality_constraints, weights,
+                        num_equality_constraints):
+    """
+    Calculates the sum of the equality constraints with inserted weights.
+
+    Parameters
+    ----------
+    equality_constraints
+    weights
+    num_equality_constraints
+
+    Returns
+    -------
+    sum of the equalities with inserted weights
+    """
+    total_loss = 0
+    for equality_constraint in equality_constraints:
+        loss = equality_constraint(weights)
+        loss = jnp.abs(loss)
+        total_loss = total_loss - loss
+
+    return total_loss
