ecea6eb10de5170b648c1af894923649f8f9683f
florianseidler <93659674+florianseidler@users.noreply.github.com>
2022-03-30 10:30:08 +0200

    80 columns

--- nlp_jax_ipm/concatenate_constraints.py
+++ nlp_jax_ipm/concatenate_constraints.py
@@ -1,18 +1,42 @@
 import jax.numpy as jnp
-from jax import grad, hessian, jacfwd, lax, jit
 
 
-def concatenate_constraints(weights, slacks, equality_constraints=None, inequality_constraints=None,
-                            num_equality_constraints=0, num_inequality_constraints=0):
+def concatenate_constraints(weights, slacks, equality_constraints=None,
+                            inequality_constraints=None,
+                            num_equality_constraints=0,
+                            num_inequality_constraints=0):
+    """
+    Returns array out of equality and inequality constraints with
+    applied weights
 
-    """Returns array out of equality and inequality constraints with applied weights"""
+    Parameters
+    ----------
+    weights
+    slacks
+    equality_constraints
+    inequality_constraints
+    num_equality_constraints
+    num_inequality_constraints
 
-    concatenated_ary = jnp.zeros((num_equality_constraints + num_inequality_constraints))
-    iter_equality = 0
-    iter_inequality = 0
-    for iter_equality in range(num_equality_constraints):
-        concatenated_ary = concatenated_ary.at[iter_equality].set(equality_constraints[iter_equality](weights))
-    for iter_inequality in range(num_inequality_constraints):
-        inequality_val = inequality_constraints[iter_inequality](weights) - slacks[iter_inequality]
-        concatenated_ary = concatenated_ary.at[iter_equality + iter_inequality].set(inequality_val)
+    Returns
+    -------
+    concatenated_ary: equality and inequality constraints with inserted values
+    """
+
+    num_zeros = num_equality_constraints + num_inequality_constraints
+    concatenated_ary = jnp.zeros(num_zeros)
+    equality_iter = 0
+
+    for equality_iter in range(num_equality_constraints):
+        concatenated_ary = (
+            concatenated_ary.at[equality_iter]
+            .set(equality_constraints[equality_iter](weights)))
+    inequality_iter = 0
+    for inequality_iter in range(num_inequality_constraints):
+        inequality_val = (inequality_constraints[inequality_iter](weights)
+                          - slacks[inequality_iter])
+        inequality_position = equality_iter + inequality_iter
+        concatenated_ary = (concatenated_ary.at[inequality_position]
+                            .set(inequality_val))
+
     return concatenated_ary
